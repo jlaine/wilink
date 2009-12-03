3f986fb3425729ebed6ecc98e8569bee57ac45a2
Jeremy Lainé <jeremy.laine@m4x.org>
2009-12-03 18:32:12 +0100

    add stub for XMPP

---
+++ src/chat.cpp
@@ -0,0 +1,77 @@
+/*
+ * wDesktop
+ * Copyright (C) 2009 Bolloré telecom
+ * See AUTHORS file for a full list of contributors.
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <QAuthenticator>
+#include <QDebug>
+#include <QList>
+#include <QStringList>
+
+#include "qxmpp/QXmppConfiguration.h"
+#include "qxmpp/QXmppLogger.h"
+#include "qxmpp/QXmppMessage.h"
+
+#include "qnetio/dns.h"
+#include "qnetio/wallet.h"
+#include "chat.h"
+
+using namespace QNetIO;
+
+Chat::Chat(QObject *parent)
+    : QXmppClient(parent)
+{
+}
+
+bool Chat::open(const QString &jid)
+{
+    QXmppConfiguration config;
+    config.setResource("BocChat");
+
+    QXmppLogger::getLogger()->setLoggingType(QXmppLogger::NONE);
+
+    /* get user and domain */
+    QStringList bits = jid.split("@");
+    if (bits.size() != 2)
+    {
+        qWarning("Invalid JID");
+        return false;
+    }
+    config.setUser(bits[0]);
+    config.setDomain(bits[1]);
+
+    /* get the server */
+    QList<ServiceInfo> results;
+    if (ServiceInfo::lookupService("_xmpp-client._tcp." + config.getDomain(), results))
+    {
+        config.setHost(results[0].hostName());
+        config.setPort(results[0].port());
+    } else {
+        config.setHost(config.getDomain());
+    }
+
+    /* get password */
+    QAuthenticator auth;
+    auth.setUser(config.getUser());
+    Wallet::instance()->onAuthenticationRequired(config.getHost(), &auth);
+    config.setPasswd(auth.password());
+
+    /* connect to server */
+    connectToServer(config);
+    return true;
+}
+
