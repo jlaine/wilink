05b79d84b937d9eea55485bd8882798593e9601a
Jeremy Lainé <jeremy.laine@bolloretelecom.eu>
2009-12-07 10:53:41 +0100

    refactor API

--- src/wireless.h
+++ src/wireless.h
@@ -1,7 +1,23 @@
 #include <QString>
 
-class WirelessInfo
+class WirelessNetwork
 {
 public:
-    static void scan(const QString &interfaceName);
+    QString ssid() const { return w_ssid; };
+    void setSsid(const QString &ssid) { w_ssid = ssid; };
+
+private:
+    QString w_ssid;
 };
+
+class WirelessInterface
+{
+public:
+    WirelessInterface(const QString &interfaceName);
+    bool isValid() const;
+    QList<WirelessNetwork> networks();
+
+private:
+    QString interfaceName;
+};
+
--- src/wireless_mac.mm
+++ src/wireless_mac.mm
@@ -1,3 +1,5 @@
+#include <QDebug>
+
 #ifdef USE_COREWLAN
 #include <CoreWLAN/CoreWLAN.h>
 #include <CoreWLAN/CWInterface.h>
@@ -8,7 +10,20 @@
 #include <Foundation/NSKeyValueObserving.h>
 #include <Foundation/NSAutoreleasePool.h>
 
+#include <SystemConfiguration/SCNetworkConfiguration.h>
+
 #include "wireless.h"
+
+inline QString cfstringRefToQstring(CFStringRef cfStringRef) {
+    QString retVal;
+    CFIndex maxLength = 2 * CFStringGetLength(cfStringRef) + 1/*zero term*/; // max UTF8
+    char *cstring = new char[maxLength];
+    if (CFStringGetCString(CFStringRef(cfStringRef), cstring, maxLength, kCFStringEncodingUTF8)) {
+        retVal = QString::fromUtf8(cstring);
+    }
+    delete cstring;
+    return retVal;
+}
 
 inline CFStringRef qstringToCFStringRef(const QString &string)
 {
@@ -21,8 +36,24 @@ inline NSString *qstringToNSString(const QString &qstr)
     return [reinterpret_cast<const NSString *>(qstringToCFStringRef(qstr)) autorelease];
 }
 
-void WirelessInfo::scan(const QString &interfaceName)
+inline QString nsstringToQString(const NSString *nsstr)
 {
+    return cfstringRefToQstring(reinterpret_cast<const CFStringRef>(nsstr));
+}
+
+WirelessInterface::WirelessInterface(const QString &name)
+    : interfaceName(name)
+{
+}
+
+bool WirelessInterface::isValid() const
+{
+    return true;
+}
+
+QList<WirelessNetwork> WirelessInterface::networks()
+{
+    QList<WirelessNetwork> results;
 #ifdef USE_COREWLAN
     NSAutoreleasePool *autoreleasepool = [[NSAutoreleasePool alloc] init];
 
@@ -31,7 +62,19 @@ void WirelessInfo::scan(const QString &interfaceName)
     NSDictionary *parametersDict = nil;
     NSArray* apArray = [NSMutableArray arrayWithArray:[currentInterface scanForNetworksWithParameters:parametersDict error:&err]];
 
+    if(!err) {
+        for(uint row=0; row < [apArray count]; row++ ) {
+            CWNetwork *apNetwork = [apArray objectAtIndex:row];
+            WirelessNetwork info;
+            info.setSsid(nsstringToQString([apNetwork ssid]));
+            results.append(info);
+        }
+    } else {
+        qWarning() << "ERROR scanning for ssids" << nsstringToQString([err localizedDescription])
+                <<nsstringToQString([err domain]);
+    }
     [autoreleasepool release];
 #endif
+    return results;
 }
 
