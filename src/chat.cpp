0b7b8de1dcf5d519b69a4f976ad3aca780d3eeb7
Jeremy Lainé <jeremy.laine@bolloretelecom.eu>
2010-01-15 10:43:28 +0100

    for now disable DNS SRV lookups

--- src/chat.cpp
+++ src/chat.cpp
@@ -195,6 +195,8 @@ bool Chat::open(const QString &jid, const QString &password)
     config.setDomain(bits[1]);
 
     /* get the server */
+    // FIXME : for now disable SRV lookups
+#ifdef USE_DNS_SRV
     QList<ServiceInfo> results;
     if (ServiceInfo::lookupService("_xmpp-client._tcp." + config.getDomain(), results))
     {
@@ -202,11 +204,12 @@ bool Chat::open(const QString &jid, const QString &password)
         config.setPort(results[0].port());
     } else {
         QString serverName = config.getDomain();
-        if (serverName == "wifirst.net")
-            serverName = "chat.wifirst.net";
         config.setHost(serverName);
         systemTrayIcon->showMessage("Could not discover chat server", QString("Connecting to server %1 instead.").arg(serverName));
     }
+#else
+    config.setHost(config.getDomain());
+#endif
 
     /* connect to server */
     statusLabel->setText(tr("Connecting.."));
