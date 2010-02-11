bde79fe3fd93e026de266f1f292840fb8c3f6939
Jeremy Lainé <jeremy.laine@bolloretelecom.eu>
2010-02-11 18:03:35 +0100

    cleanup

--- src/chat_dialog.cpp
+++ src/chat_dialog.cpp
@@ -17,13 +17,9 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include <QContextMenuEvent>
-#include <QDebug>
-#include <QEvent>
 #include <QDateTime>
 #include <QLabel>
 #include <QLayout>
-#include <QLineEdit>
 #include <QList>
 #include <QMessageBox>
 #include <QPushButton>
