20e7b6327d7c0553ea8e3d18279aa048fee7fb1d
Jeremy Lainé <jeremy.laine@bolloretelecom.eu>
2010-02-12 14:34:43 +0100

    don't invite if already in chat room

--- src/chat.cpp
+++ src/chat.cpp
@@ -445,7 +445,9 @@ void Chat::messageReceived(const QXmppMessage &msg)
         {
             const QString contactName = rosterModel->contactName(bareJid);
             const QString roomJid = msg.getExtension().attribute("jid");
-            if (!roomJid.isEmpty() && QMessageBox::question(this,
+            if (!roomJid.isEmpty() &&
+                !chatDialogs.contains(roomJid) &&
+                QMessageBox::question(this,
                     tr("Invitation from %1").arg(contactName),
                     tr("%1 has asked to add you to join the '%2' chat room. Do you accept?").arg(contactName, roomJid),
                     QMessageBox::Yes | QMessageBox::No, QMessageBox::Yes) == QMessageBox::Yes)
