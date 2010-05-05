f9aa21215a148421ff3676b41c5b1fead9137a35
Jeremy Lainé <jeremy.laine@m4x.org>
2010-05-05 23:49:35 +0200

    move chat rooms entirely to plugin

--- src/plugins/rooms.h
+++ src/plugins/rooms.h
@@ -20,14 +20,26 @@
 #ifndef __WILINK_ROOMS_H__
 #define __WILINK_ROOMS_H__
 
-#include <QObject>
+#include <QDialog>
+#include <QMessageBox>
+
+#include "chat_conversation.h"
 
 class Chat;
+class ChatRosterModel;
+class QLineEdit;
+class QListWidget;
+class QListWidgetItem;
 class QMenu;
 class QModelIndex;
 class QPushButton;
+class QTableWidget;
+class QXmppClient;
+class QXmppDiscoveryIq;
+class QXmppIq;
 class QXmppMessage;
 class QXmppMucOwnerIq;
+class QXmppPresence;
 
 class ChatRoomWatcher : public QObject
 {
@@ -53,6 +65,92 @@ private:
     Chat *chat;
     QString chatRoomServer;
     QPushButton *roomButton;
+};
+
+class ChatRoom : public ChatConversation
+{
+    Q_OBJECT
+
+public:
+    ChatRoom(QXmppClient *xmppClient, ChatRosterModel *chatRosterModel, const QString &jid, QWidget *parent = NULL);
+    ChatRosterItem::Type objectType() const;
+
+protected:
+    virtual void sendMessage(const QString &text);
+
+private slots:
+    void discoveryIqReceived(const QXmppDiscoveryIq &disco);
+    void join();
+    void leave();
+    void disconnected();
+    void messageReceived(const QXmppMessage &msg);
+    void presenceReceived(const QXmppPresence &msg);
+
+private:
+    QString chatLocalJid;
+    QXmppClient *client;
+    bool joined;
+    bool notifyMessages;
+    ChatRosterModel *rosterModel;
+};
+
+class ChatRoomMembers : public QDialog
+{
+    Q_OBJECT
+
+public:
+    ChatRoomMembers(QXmppClient *client, const QString &roomJid, QWidget *parent);
+
+protected slots:
+    void addMember();
+    void removeMember();
+    void iqReceived(const QXmppIq &iq);
+    void submit();
+
+private:
+    void addEntry(const QString &jid, const QString &affiliation);
+    QString chatRoomJid;
+    QXmppClient *client;
+    QXmppElement form;
+    QTableWidget *tableWidget;
+    QMap<QString, QString> initialMembers;
+    QMap<QString, QString> affiliations;
+};
+
+class ChatRoomInvitePrompt : public QMessageBox
+{
+    Q_OBJECT
+
+public:
+    ChatRoomInvitePrompt(const QString &contactName, const QString &jid, QWidget *parent = 0);
+
+signals:
+    void roomSelected(const QString &jid);
+
+private slots:
+    void slotButtonClicked(QAbstractButton *button);
+
+private:
+    QString m_jid;
+};
+
+class ChatRoomPrompt : public QDialog
+{
+    Q_OBJECT
+
+public:
+    ChatRoomPrompt(QXmppClient *client, const QString &roomServer, QWidget *parent = 0);
+    QString textValue() const;
+
+protected slots:
+    void discoveryIqReceived(const QXmppDiscoveryIq &disco);
+    void itemClicked(QListWidgetItem * item);
+    void validate();
+
+private:
+    QLineEdit *lineEdit;
+    QListWidget *listWidget;
+    QString chatRoomServer;
 };
 
 #endif
