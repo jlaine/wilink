3126bbae067465efd11b70a174ca9ef1f8a3246b
Jeremy Lainé <jeremy.laine@m4x.org>
2010-09-20 13:19:14 +0200

    simplify main

--- src/mobile/main.cpp
+++ src/mobile/main.cpp
@@ -17,27 +17,9 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include <signal.h>
-
 #include <QApplication>
-#include <QLocale>
-#include <QSettings>
-#include <QSystemTrayIcon>
-#include <QTimer>
-#include <QTranslator>
-//#include <QtPlugin>
 
 #include "window.h"
-
-static int aborted = 0;
-static void signal_handler(int)
-{
-    if (aborted)
-        exit(1);
-
-    qApp->quit();
-    aborted = 1;
-}
 
 int main(int argc, char *argv[])
 {
@@ -45,24 +27,6 @@ int main(int argc, char *argv[])
     QApplication app(argc, argv);
     app.setApplicationName("wiLite");
     app.setApplicationVersion("0.1.0");
-
-    /* Load translations */
-    QString localeName = QLocale::system().name().split("_").first();
-
-    QTranslator qtTranslator;
-    qtTranslator.load(QString(":/qt_%1.qm").arg(localeName));
-    app.installTranslator(&qtTranslator);
-
-    QTranslator appTranslator;
-    appTranslator.load(QString(":/%1.qm").arg(localeName));
-    app.installTranslator(&appTranslator);
-
-    /* Install signal handler */
-    signal(SIGINT, signal_handler);
-#ifdef SIGKILL
-    signal(SIGKILL, signal_handler);
-#endif
-    signal(SIGTERM, signal_handler);
 
     /* Show main window */
     MainWindow window;
