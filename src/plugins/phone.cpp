51b67ca06bd110a0a8e1099cd0268b24e8ec462c
Jeremy Lainé <jeremy.laine@m4x.org>
2010-11-15 03:06:43 +0100

    add phone plugin

---
+++ src/plugins/phone.cpp
@@ -0,0 +1,58 @@
+/*
+ * wiLink
+ * Copyright (C) 2009-2010 Bolloré telecom
+ * See AUTHORS file for a full list of contributors.
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "chat.h"
+#include "chat_plugin.h"
+#include "chat_roster.h"
+
+#include "phone/sip.h"
+#include "phone.h"
+ 
+PhonePanel::PhonePanel(ChatClient *xmppClient, QWidget *parent)
+    : ChatPanel(parent),
+    client(xmppClient)
+{
+}
+
+// PLUGIN
+
+class PhonePlugin : public ChatPlugin
+{
+public:
+    bool initialize(Chat *chat);
+};
+
+bool PhonePlugin::initialize(Chat *chat)
+{
+    const QString domain = chat->client()->configuration().domain();
+    if (domain != "wifirst.net")
+        return false;
+
+#if 0
+    PhoneWatcher *watcher = new PhoneWatcher(chat);
+
+    /* add roster hooks */
+    connect(chat, SIGNAL(rosterMenu(QMenu*, QModelIndex)),
+            watcher, SLOT(rosterMenu(QMenu*, QModelIndex)));
+#endif
+    return true;
+}
+
+Q_EXPORT_STATIC_PLUGIN2(phone, PhonePlugin)
+
