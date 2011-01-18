58f8c2c8c8e07c61efd11f37b60c6a7a3e3507c0
Jeremy Lainé <jeremy.laine@bolloretelecom.eu>
2011-01-18 12:53:01 +0100

    add stub for preferences window

---
+++ src/chat_preferences.cpp
@@ -0,0 +1,85 @@
+/*
+ * wiLink
+ * Copyright (C) 2009-2011 Bolloré telecom
+ * See AUTHORS file for a full list of contributors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <QDialogButtonBox>
+#include <QLayout>
+#include <QListWidget>
+#include <QSplitter>
+#include <QStackedWidget>
+
+#include "chat_preferences.h"
+
+class ChatPreferencesPrivate
+{
+public:
+    QListWidget *tabList;
+    QStackedWidget *tabStack;
+};
+
+/** Constructs a new preferences dialog.
+ *
+ * @param parent
+ */
+ChatPreferences::ChatPreferences(QWidget *parent)
+    : QDialog(parent),
+      d(new ChatPreferencesPrivate)
+{
+    QVBoxLayout *layout = new QVBoxLayout;
+
+    // splitter
+    QSplitter *splitter = new QSplitter;
+    
+    d->tabList = new QListWidget;
+    splitter->addWidget(d->tabList);
+    splitter->setStretchFactor(0, 0);
+
+    d->tabStack = new QStackedWidget;
+    splitter->addWidget(d->tabStack);
+    splitter->setStretchFactor(1, 1);
+
+    layout->addWidget(splitter);
+
+    // buttons
+    QDialogButtonBox *buttonBox = new QDialogButtonBox(QDialogButtonBox::Ok | QDialogButtonBox::Cancel);
+    connect(buttonBox, SIGNAL(accepted()), this, SLOT(validate()));
+    connect(buttonBox, SIGNAL(rejected()), this, SLOT(reject()));
+    layout->addWidget(buttonBox);
+
+    setLayout(layout);
+    setWindowTitle(tr("Preferences"));
+}
+
+/** Destroys a preferences dialog.
+ */
+ChatPreferences::~ChatPreferences()
+{
+    delete d;
+}
+
+/** Validates and applies the new preferences.
+ */
+void ChatPreferences::validate()
+{
+    accept();
+}
+
+ChatPreferencesTab::ChatPreferencesTab()
+{
+}
+
---
+++ src/chat_preferences.h
@@ -0,0 +1,53 @@
+/*
+ * wiLink
+ * Copyright (C) 2009-2011 Bolloré telecom
+ * See AUTHORS file for a full list of contributors.
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __WILINK_CHAT_PREFERENCES_H__
+#define __WILINK_CHAT_PREFERENCES_H__
+
+#include <QDialog>
+
+class ChatPreferencesPrivate;
+
+/** ChatPreferences is the base class for all settings tabs.
+ */
+class ChatPreferences : public QDialog
+{
+    Q_OBJECT
+
+public:
+    ChatPreferences(QWidget *parent = 0);
+    ~ChatPreferences();
+
+private slots:
+    void validate();
+
+private:
+    ChatPreferencesPrivate * const d;
+};
+
+class ChatPreferencesTab : public QWidget
+{
+    Q_OBJECT
+
+public:
+    ChatPreferencesTab();
+};
+
+
+#endif
