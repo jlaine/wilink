d408e911b3b9d80e0b4b6c1bf8499f2598ab1ae8
Jeremy Lainé <jeremy.laine@bolloretelecom.eu>
2011-05-03 11:08:39 +0200

    cleanup

--- src/plugins/calls.cpp
+++ src/plugins/calls.cpp
@@ -273,9 +273,6 @@ CallWidget::CallWidget(QXmppCall *call, ChatRosterModel *rosterModel, QGraphicsI
 
 CallWidget::~CallWidget()
 {
-    //audioModeChanged(QIODevice::NotOpen);
-    videoModeChanged(QIODevice::NotOpen);
-
     // stop tone
     if (m_soundId)
         wApp->soundPlayer()->stop(m_soundId);
