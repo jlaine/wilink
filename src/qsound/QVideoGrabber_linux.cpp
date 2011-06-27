4a1e92ac87c8cc19e92fa181b82fe48f8ff5dded
Jeremy Lainé <jeremy.laine@m4x.org>
2011-06-27 16:19:42 +0200

    support RGB24 input

--- src/qsound/QVideoGrabber_linux.cpp
+++ src/qsound/QVideoGrabber_linux.cpp
@@ -33,6 +33,8 @@
 static QXmppVideoFrame::PixelFormat v4l_to_qxmpp_PixelFormat(__u32 pixelFormat)
 {
     switch (pixelFormat) {
+    case V4L2_PIX_FMT_RGB24:
+        return QXmppVideoFrame::Format_RGB24;
     case V4L2_PIX_FMT_UYVY:
         return QXmppVideoFrame::Format_UYVY;
     case V4L2_PIX_FMT_YUYV:
@@ -135,7 +137,9 @@ bool QVideoGrabberPrivate::open()
     format.fmt.pix.width = videoFormat.frameWidth();
     format.fmt.pix.height = videoFormat.frameHeight();
     format.fmt.pix.field = V4L2_FIELD_INTERLACED;
-    if (videoFormat.pixelFormat() == QXmppVideoFrame::Format_UYVY)
+    if (videoFormat.pixelFormat() == QXmppVideoFrame::Format_RGB24)
+        format.fmt.pix.pixelformat = V4L2_PIX_FMT_RGB24;
+    else if (videoFormat.pixelFormat() == QXmppVideoFrame::Format_UYVY)
         format.fmt.pix.pixelformat = V4L2_PIX_FMT_UYVY;
     else if (videoFormat.pixelFormat() == QXmppVideoFrame::Format_YUYV)
         format.fmt.pix.pixelformat = V4L2_PIX_FMT_YUYV;
