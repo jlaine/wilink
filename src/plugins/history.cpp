1033164818f3626e3cfb694e13436bca9c0f00be
Jeremy Lainé <jeremy.laine@m4x.org>
2011-07-05 10:38:19 +0200

    relax duplicate comparison

--- src/plugins/history.cpp
+++ src/plugins/history.cpp
@@ -198,7 +198,7 @@ void HistoryModel::addMessage(const HistoryMessage &message)
             if (message.archived &&
                 message.jid == curMessage->jid &&
                 message.body == curMessage->body &&
-                message.date == curMessage->date)
+                message.date.msecsTo(curMessage->date) < 1000)
                 return;
 
             // we use greater or equal comparison (and not strictly greater) dates
