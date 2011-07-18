ebe5be122439409db343a0665dd5d3ef5ceb9e1c
Jeremy Lainé <jeremy.laine@m4x.org>
2011-07-18 13:42:42 +0200

    add stub for echo canceller tester

---
+++ src/qsound/tests/echo.cpp
@@ -0,0 +1,62 @@
+/*
+ * wiLink
+ * Copyright (C) 2009-2011 Bolloré telecom
+ * See AUTHORS file for a full list of contributors.
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <cstdlib>
+#include <csignal>
+
+#include <QApplication>
+#include <QBuffer>
+
+#include "QSoundPlayer.h"
+#include "QSoundStream.h"
+#include "echo.h"
+
+EchoTester::EchoTester(QObject *parent)
+    : QObject(parent)
+{
+    m_buffer = new QBuffer(this);
+
+    m_player = new QSoundPlayer(this);
+
+    m_stream = new QSoundStream(m_player);
+    m_stream->setDevice(m_buffer);
+    m_stream->setFormat(1, 8000);
+}
+
+static int aborted = 0;
+static void signal_handler(int sig)
+{
+    if (aborted)
+        exit(1);
+
+    qApp->quit();
+    aborted = 1;
+}
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+    /* Install signal handler */
+    signal(SIGINT, signal_handler);
+    signal(SIGTERM, signal_handler);
+
+    EchoTester tester;
+
+    return app.exec();
+}
