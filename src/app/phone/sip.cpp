2839e4283f97880392c71855018c9211b87be1ad
Jeremy Lainé <jeremy.laine@m4x.org>
2012-04-16 21:36:09 +0200

    revert

--- src/app/phone/sip.cpp
+++ src/app/phone/sip.cpp
@@ -504,12 +504,11 @@ void SipCallPrivate::setState(QXmppCall::State newState)
         onStateChanged();
         emit q->stateChanged(state);
 
-        if (state == QXmppCall::ActiveState) {
-            startStamp = QDateTime::currentDateTime();
+        if (state == QXmppCall::ActiveState)
             emit q->connected();
-        } else if (state == QXmppCall::FinishedState) {
+        else if (state == QXmppCall::FinishedState)
+        {
             q->debug(QString("SIP call %1 finished").arg(QString::fromUtf8(id)));
-            finishStamp = QDateTime::currentDateTime();
             emit q->finished();
         }
     }
