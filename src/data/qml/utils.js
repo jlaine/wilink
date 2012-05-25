3d2065f472b0273aa36a43267edfe9e396334527
Jeremy Lainé <jeremy.laine@m4x.org>
2012-05-25 14:34:57 +0200

    add js version of QXmppUtils::datetimeFromString

--- src/data/qml/utils.js
+++ src/data/qml/utils.js
@@ -19,6 +19,21 @@
 
 .pragma library
 
+function datetimeFromString(text)
+{
+    var cap = text.match(/([0-9]{4})-([0-9]{2})-([0-9]{2})T([0-9]{2}):([0-9]{2}):([0-9]{2})Z/);
+    if (cap) {
+        var dt = new Date();
+        dt.setUTCFullYear(parseInt(cap[1]));
+        dt.setUTCMonth(parseInt(cap[2]) - 1);
+        dt.setUTCDate(parseInt(cap[3]));
+        dt.setUTCHours(parseInt(cap[4]));
+        dt.setUTCMinutes(parseInt(cap[5]));
+        dt.setUTCSeconds(parseInt(cap[6]));
+        return dt;
+    }
+}
+
 function escapeRegExp(text) {
     return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
 }
