433db4ea92d2762de38f450e063ebbe17a533ce4
Jeremy Lainé <jeremy.laine@m4x.org>
2012-06-01 12:02:20 +0200

    build fix for mac os lion

--- src/app/application_mac.mm
+++ src/app/application_mac.mm
@@ -17,9 +17,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include <AppKit/AppKit.h>
-#include <Foundation/NSAutoreleasePool.h>
-
 #include <QApplication>
 #include "window.h"
 
