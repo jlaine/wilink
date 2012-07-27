d11b4069f6c5a1b741b53179254940ce9dd03599
Jeremy Lainé <jeremy.laine@m4x.org>
2012-07-27 11:24:58 +0200

    bump version

--- src/app/window.cpp
+++ src/app/window.cpp
@@ -58,7 +58,7 @@ CustomWindow::CustomWindow(QtLocalPeer *peer, QWidget *parent)
     Q_ASSERT(check);
 
     // declare QML roots
-    d->qmlRoots << QUrl("https://download.wifirst.net/wiLink/2.3.900/qml/");
+    d->qmlRoots << QUrl("https://download.wifirst.net/wiLink/2.3.907/qml/");
     d->qmlRoots << QUrl("qrc:/qml/");
 
     // create declarative view
